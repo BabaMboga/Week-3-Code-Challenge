643c483b6a935789a28bd52b762345bf05dd377d
AyimWilliam <odhisayim@gmail.com>
2023-03-26 15:32:44 +0300

    First complete prototype

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () =>{
 const animalList = document.querySelector('#animal-list');
 const animalDetails = document.querySelector('#animal-details');
 
@@ -10,6 +11,29 @@ fetch("http://localhost:3000/characters")
     characters.forEach(function character(){
         const li = document.createElement('li');
         li.textContent = character.name;
+        li.addEventListener('click', ()=>{
+            fetch(`http://localhost:3000/characters/${character.id}`)
+            .then(response => response.json())
+          .then(characterDetails => {
+            // Populate animal details with character details
+            animalDetails.innerHTML = `
+              <img src="${characterDetails.image}">
+              <h2>${characterDetails.name}</h2>
+              <p>Votes: ${characterDetails.votes}</p>
+              <button id="vote-button">Vote</button>
+            `;
+            // Add event listener to vote button
+            const voteButton = document.querySelector('#vote-button');
+            voteButton.addEventListener('click', () => {
+              characterDetails.votes++;
+              animalDetails.querySelector('p').textContent = `Votes: ${characterDetails.votes}`;
+            });
+            // Show animal details
+            animalDetails.style.display = 'block';
+          });
+      });
+      animalList.appendChild(li);
+        })
     })
     
 
